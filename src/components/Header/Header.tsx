cf2638c624f4dfaa1e01d1c5fded6e71a378676e
Nihat04 <aliev.nihat4@gmail.com>
2024-07-14 00:44:05 +0500

    header corresponds to pixel perfect make up

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,22 +7,17 @@ import classNames from 'classnames';
 const Header = () => {
     return (
         <header className={styles['header']}>
-            <div
-                className={classNames(
-                    globalStyles['container'],
-                    styles['container']
-                )}
-            >
+            <div className={styles['header__logo--container']}>
                 <img
                     className={styles['header__logo']}
                     src={logo}
                     alt="логотип"
                 />
-                <h1 className={styles['header__header-text']}>
-                    STUDENTS by{' '}
-                    <span className={styles['color--blue']}>Nihat04</span>
-                </h1>
             </div>
+            <h1 className={styles['header__header-text']}>
+                STUDENTS by{' '}
+                <span className={styles['color--blue']}>Nihat04</span>
+            </h1>
         </header>
     );
 };
